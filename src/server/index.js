d887dbeb525644d0b9366a8a07dfa4742c4466a0
Chris <94713188+Itc47p@users.noreply.github.com>
2025-04-23 22:19:12 -0400

    Update index.js
    code cleanup

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,6 @@ const path = require('path')
 const app = express()
 const port = 3000
 const API_KEY = process.env.API_KEY
-console.log('API_KEY', API_KEY);
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -16,11 +15,11 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 
 require('dotenv').config();
 
-const base = 'https://api.nasa.gov';
+const apiUrlBase = 'https://api.nasa.gov';
 
 app.get('/apod', async (req, res) => {
     try {
-        let image = await fetch(`${base}/planetary/apod?api_key=${API_KEY}`)
+        let image = await fetch(`${apiUrlBase}/planetary/apod?api_key=${API_KEY}`)
             .then(res => res.json());
         res.send({ image });
     } catch (err) {
@@ -33,34 +32,13 @@ app.get('/apod', async (req, res) => {
 
 app.get('/rovers/:name', async (req, res) => {
     try {
-        let photos = await fetch(`${base}/mars-photos/api/v1/rovers/${req.params.name}/latest_photos?api_key=${API_KEY}`)
+        let data = await fetch(`${apiUrlBase}/mars-photos/api/v1/rovers/${req.params.name}/latest_photos?api_key=${API_KEY}`)
             .then(res => res.json());
-        res.send({ image });
+        const limitedData = data.latest_photos ? data.latest_photos.slice(0, 5) : [];
+        res.send({ data: limitedData });
     } catch (err) {
-        console.log('error', err);
+        console.error('Error fetching rover data:', err); 
+        res.status(500).send('Internal Server Error');
     }
-})
-
-
-// app.get('/manifests/:rover', async (req, res) => {
-//     const rover = req.params.rover;
-//     const apiUrl = `https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${API_KEY}`;
-//     console.log('Fetching manifest data from:', apiUrl);
-//     try {
-//         const response = await fetch(apiUrl);
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-//         const data = await response.json();
-//         console.log('Received data:', data); // Log the received data
-//         const launch_date = data.photo_manifest.launch_date;
-//         const landing_date = data.photo_manifest.landing_date;
-//         const status = data.photo_manifest.status;
-//         res.send({ rover, launch_date, landing_date, status });
-//     } catch (err) {
-//         console.log('get manifest error:', err);
-//         res.status(500).send('Error fetching rover data');
-//     }
-// });
-
-app.listen(port, () => console.log(`Mars Dasboard listening on port ${port}!`))
+});
+app.listen(port, () => console.log(`Dasboard listening on port ${port}`))
